b395e776f34087b96dc5db7cf85e626fb466b1d0
Alan Licerio <alanlicerio@Alans-MacBook-Pro.local>
2020-09-02 22:01:10 -0600

    Started working on the lab.

---
+++ wordCount.py
@@ -0,0 +1,11 @@
+# Intro to Python Lab
+# Operating Systems
+# Created by: Alan Licerio
+# Last modified: 09-02-20
+
+def countWords(fileName, numWords, listWords):
+    # Hash function or using a dictionary to count occurences.
+    return
+
+if __name__ == "__main__":
+    fileName = input("Enter name of file that will be read\n")
